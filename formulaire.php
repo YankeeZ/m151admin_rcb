3565e5a0428fced3ce135572a46ff83eeeb37895
Carvalho <brunheta.ruben@gmail.com>
2015-08-26 14:47:18 +0200

    formulaire

---
+++ formulaire.php
@@ -0,0 +1,35 @@
+<?php 
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Formulaire</title>
+  <link rel="stylesheet" href="css.css">
+  <script src="script.js"></script>
+</head>
+<body>
+    <div id="divPrin">
+        <h1>Formulaire</h1>
+        <form>
+            Nom : <br/>
+            <input type="text" name="Nom"><br/>
+            Prénom : <br/>
+            <input type="text" name="Prenom"><br/>
+            Date de Naissance : <br/>
+            <input type="date" name="date"><br/>
+            Email : <br/>
+            <input type="email" name="email"><br/>
+            Pseudo : <br/>
+            <input type="text" name="pseudo"><br/>
+            Password : <br/>
+            <input type="password" name="password"><br/>
+            Description : <br/>
+            <textarea rows="4" cols="50">Description</textarea>
+        </form>
+        
+    </div>
+</body>
+</html>
